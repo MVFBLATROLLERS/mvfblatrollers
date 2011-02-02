d23b7426426e000362783e11d6a37f0a70e7d89a
d <d@g.com>
2011-02-01 21:57:55 -0800

    fully functioning login/register, now to work on entries and search

--- include/error.php
+++ include/error.php
@@ -1,26 +1,22 @@
 <?php
 
-$error = '';
-
-function print_err($string){
-    $GLOBALS['error'] .= $string . "<br />\n";
-}
+$error = FALSE;
 
 function e_var_export($var){
     $return = print_r($var, true);
     $return = str_replace(" ", "&nbsp;", $return);
     $return = nl2br($return, true);
-    print_err($return);
-}
-
-function print_errors(){
-    echo("<div id=\"error\">".$GLOBALS['error']."</div>");
+    user_error($return);
 }
 
 function check_errors(){
-    if(empty($GLOBALS['error']))
-        return FALSE;
-    else
-        return TRUE;
+	return $GLOBALS['error'];
 }
+
+function user_error_handler($errorno, $errstr, $errfile, $errline){
+	echo('<span id="error">'.$errstr.'</span><br />');
+	$GLOBALS['error'] = TRUE;
+}
+
+set_error_handler("user_error_handler", E_USER_NOTICE | E_USER_WARNING);
 ?>
